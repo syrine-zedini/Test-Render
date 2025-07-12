d7ba884b425d05dea66f6b7706cfab9a296d8e3c
syrine zedini <syrinezeddini097@gmail.com>
2025-07-12 18:21:31 +0100

    fixing render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-const http= require('http');
-const app= require('./app');
-app.set('port',process.env.PORT || 3000);
-const server=http.createServer(app);
+const http = require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
 
-server.listen(process.env.PORT || 4000);
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
